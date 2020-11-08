787785496e64c004e46753014a80998678e208a9
BChristianV <bchristianv@icloud.com>
2020-11-08 15:29:04 -0800

    Formatting fixes for outputs file.

--- outputs.tf
+++ outputs.tf
@@ -14,50 +14,50 @@ output "az_public_cidrs" {
 
 output "cidr_block" {
   description = "The IP block of the VPC in CIDR notation"
-  value = aws_vpc.vpc.cidr_block
+  value       = aws_vpc.vpc.cidr_block
 }
 
 output "id" {
   description = "The ID of the VPC"
-  value = aws_vpc.vpc.id
+  value       = aws_vpc.vpc.id
 }
 
 output "internal_dns_zone_id" {
   description = "The ID of the internal DNS zone"
-  value = aws_route53_zone.internal_dns_zone.zone_id
+  value       = aws_route53_zone.internal_dns_zone.zone_id
 }
 
 output "internal_dns_zone_nameservers" {
   description = "A list of the nameservers for the internal DNS zone"
-  value = aws_route53_zone.internal_dns_zone.name_servers
+  value       = aws_route53_zone.internal_dns_zone.name_servers
 }
 
 output "internet_gw_id" {
   description = "The ID of the internet gateway"
-  value = [for gw in aws_internet_gateway.igw : gw.id]
+  value       = [for gw in aws_internet_gateway.igw : gw.id]
 }
 
 output "nat_gw_ids" {
   description = "A list of the NAT gateway ID's"
-  value = aws_nat_gateway.nat_gws[*].id
+  value       = aws_nat_gateway.nat_gws[*].id
 }
 
 output "private_route_table_ids" {
   description = "A list of the private route table ID's"
-  value = values(aws_route_table.private_rt)[*].id
+  value       = values(aws_route_table.private_rt)[*].id
 }
 
 output "private_subnet_ids" {
   description = "A list of the private subnet ID's"
-  value = values(aws_subnet.private_subnets)[*].id
+  value       = values(aws_subnet.private_subnets)[*].id
 }
 
 output "public_route_table_ids" {
   description = "A list of the public route table ID's"
-  value = [for rt in aws_route_table.public_rt : rt.id]
+  value       = [for rt in aws_route_table.public_rt : rt.id]
 }
 
 output "public_subnet_ids" {
   description = "A list of the public subnet ID's"
-  value = values(aws_subnet.public_subnets)[*].id
+  value       = values(aws_subnet.public_subnets)[*].id
 }
