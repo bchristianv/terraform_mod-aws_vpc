a6f1d6e39640c0057127aa623e8bf10a2b41a00f
BChristianV <bchristianv@icloud.com>
2020-11-12 08:36:23 -0800

    Update example config for 1.0.3 release.

--- examples/az2-pri2-pub2.tf
+++ examples/az2-pri2-pub2.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source  = "github.com/bchristianv/terraform_mod-aws_vpc?ref=1.0.2"
+  source  = "github.com/bchristianv/terraform_mod-aws_vpc?ref=1.0.3"
 
   aws_region = "us-west-2"
 
