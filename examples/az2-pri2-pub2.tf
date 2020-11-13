c32f354e7259583edbe994d4cbf7a0ef3083c717
BChristianV <bchristianv@icloud.com>
2020-11-13 09:18:24 -0800

    Increment example config for 1.1.0 release.

--- examples/az2-pri2-pub2.tf
+++ examples/az2-pri2-pub2.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "github.com/bchristianv/terraform_mod-aws_vpc?ref=1.0.3"
+  source = "github.com/bchristianv/terraform_mod-aws_vpc?ref=1.1.0"
 
   aws_region = "us-west-2"
 
